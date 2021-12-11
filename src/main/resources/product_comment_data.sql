ed8560479972dd8fa32e621650642bfa1337ab8b
Tarık COŞKUN <tarikcoskun@hotmail.com.tr>
2021-12-12 02:32:22 +0300

    SQL sample data and settings were edited.

---
+++ src/main/resources/product_comment_data.sql
@@ -0,0 +1,29 @@
+INSERT INTO public.PRODUCT_COMMENTS(id, creation_date, text, id_product, id_user)
+VALUES (1, now(),
+        'The European languages are members of the same family. Their separate existence is a myth. For scien',
+        2, 3);
+
+INSERT INTO public.PRODUCT_COMMENTS(id, creation_date, text, id_product, id_user)
+VALUES (2, now(),
+        'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large.',
+        2, 7);
+
+INSERT INTO public.PRODUCT_COMMENTS(id, creation_date, text, id_product, id_user)
+VALUES (3, now(),
+        'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was.',
+        6, 1);
+
+INSERT INTO public.PRODUCT_COMMENTS(id, creation_date, text, id_product, id_user)
+VALUES (4, now(),
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+        7, 7);
+
+INSERT INTO public.PRODUCT_COMMENTS(id, creation_date, text, id_product, id_user)
+VALUES (5, now(),
+        'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Qu',
+        5, 6);
+
+INSERT INTO public.PRODUCT_COMMENTS(id, creation_date, text, id_product, id_user)
+VALUES (6, now(),
+        'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids',
+        6, 3);
---
+++ src/main/resources/user_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.USERS( id, name , surname, email, telephone_number) VALUES (	1	,'Tarık'	,'COŞKUN'	,'tarikcoskun@hotmail.com.tr'	,'05548960682');
+INSERT INTO public.USERS( id, name , surname, email, telephone_number) VALUES (	2	,'Ali'		,'KELEŞ'	,'alikeles@hotmail.com'			,'05356987416');
+INSERT INTO public.USERS( id, name , surname, email, telephone_number) VALUES (	3	,'Beyza'	,'ÇELİK'	,'beyzac@outlook.com.tr'		,'05421548937');
+INSERT INTO public.USERS( id, name , surname, email, telephone_number) VALUES (	4	,'Elif'		,'KATİP'	,'katipelif@gmail.com'			,'05563178965');
+INSERT INTO public.USERS( id, name , surname, email, telephone_number) VALUES (	5	,'Ahmet'	,'ASLAN'	,'aaslan@yahoo.com'				,'05555556987');
+INSERT INTO public.USERS( id, name , surname, email, telephone_number) VALUES (	6	,'Kerem'	,'KİRAZ'	,'keremkiraz@hotmail.com.tr'	,'05486587215');
+INSERT INTO public.USERS( id, name , surname, email, telephone_number) VALUES (	7	,'Seray'	,'AKYOL'	,'serayyyakyol@hotmail.com'		,'05386649987');
